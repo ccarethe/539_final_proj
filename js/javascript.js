98f1fdded2143c5b3fe5b17ec72e7d653be44b08
ccarethe <ccarethe@umich.edu>
2024-04-13 22:40:52 -0400

    Major updates

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+const items = document.querySelectorAll('.ingredient');
+
+items.forEach(item => {
+    item.addEventListener('click', function() {
+        if (this.style.textDecoration === 'line-through') {
+            this.style.textDecoration = 'none';
+            this.style.color = "#fff";
+            this.style.backgroundColor = "#525d9c";
+        } else {
+            this.style.textDecoration = 'line-through';
+            this.style.color = "#C8C8C8";
+            this.style.backgroundColor = "#eff2fd";
+        }
+    });
+});
